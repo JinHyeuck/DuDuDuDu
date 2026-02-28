06fec8e7cd766f1465d2fc23fd9dc99044d068d3
OjinHyeok <ojh5266@gameberry.co.kr>
2026-02-28 23:51:21 +0900

    버튼 연출 수정

--- DuDuDuDU_Project/Assets/Scripts/Lobby/LobbyBottomBtn.cs
+++ DuDuDuDU_Project/Assets/Scripts/Lobby/LobbyBottomBtn.cs
@@ -13,6 +13,11 @@ namespace OJ
         [SerializeField] private float _maxwidth = 320f;
         [SerializeField] private Transform _minWidthObj;
         [SerializeField] private Transform _maxWidthObj;
+        [SerializeField] private float _maxWidthObj_HideScale = 0.7f;
+        [SerializeField] private float _maxWidthObj_HideYpos = -8.5f;
+        [SerializeField] private float _maxWidthObj_ShowScale = 1.0f;
+        [SerializeField] private float _maxWidthObj_ShowYpos = 38.69f;
+
         [SerializeField] private Button _button;
 
         public LobbyTab _tab;
@@ -59,13 +64,25 @@ namespace OJ
         {
             float targetWidth = isSelected ? _maxwidth : _minwidth;
             float duration = Mathf.Max(0f, _widthChangeDuration);
+            float targetScale = isSelected ? _maxWidthObj_ShowScale : _maxWidthObj_HideScale;
+            float targetY = isSelected ? _maxWidthObj_ShowYpos : _maxWidthObj_HideYpos;
 
             if (_minWidthObj != null) _minWidthObj.gameObject.SetActive(!isSelected);
-            if (_maxWidthObj != null) _maxWidthObj.gameObject.SetActive(isSelected);
+
+            float startScale = targetScale;
+            float startY = targetY;
+            if (_maxWidthObj != null)
+            {
+                if (isSelected) _maxWidthObj.gameObject.SetActive(true);
+                startScale = _maxWidthObj.localScale.x;
+                startY = _maxWidthObj.localPosition.y;
+            }
 
             if (duration <= 0f)
             {
                 if (_rect != null) SetWidth(targetWidth);
+                SetMaxWidthObjVisual(targetScale, targetY);
+                if (_maxWidthObj != null) _maxWidthObj.gameObject.SetActive(isSelected);
                 _widthRoutine = null;
                 yield break;
             }
@@ -79,11 +96,16 @@ namespace OJ
                 float widthT = Mathf.Clamp01(elapsed / duration);
 
                 if (_rect != null) SetWidth(Mathf.Lerp(startWidth, targetWidth, widthT));
+                SetMaxWidthObjVisual(
+                    Mathf.Lerp(startScale, targetScale, widthT),
+                    Mathf.Lerp(startY, targetY, widthT));
 
                 yield return null;
             }
 
             if (_rect != null) SetWidth(targetWidth);
+            SetMaxWidthObjVisual(targetScale, targetY);
+            if (_maxWidthObj != null) _maxWidthObj.gameObject.SetActive(isSelected);
 
             _widthRoutine = null;
         }
@@ -93,6 +115,20 @@ namespace OJ
             Vector2 size = _rect.sizeDelta;
             size.x = width;
             _rect.sizeDelta = size;
+        }
+
+        private void SetMaxWidthObjVisual(float scale, float ypos)
+        {
+            if (_maxWidthObj == null) return;
+
+            Vector3 localScale = _maxWidthObj.localScale;
+            localScale.x = scale;
+            localScale.y = scale;
+            _maxWidthObj.localScale = localScale;
+
+            Vector3 localPos = _maxWidthObj.localPosition;
+            localPos.y = ypos;
+            _maxWidthObj.localPosition = localPos;
         }
 
     }
